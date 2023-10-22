81bbad86bcc4bf06c9a8cfee811d2d83dcd366ec
pgiang2001 <pgisme07@gmail.com>
2023-10-22 12:58:17 +0700

    create signup user

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -16,3 +17,8 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use(express.json())
+
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
